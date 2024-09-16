e193a8ceb753083162bba667bf799a8993576b99
prachisingh3515 <prachisingh35150@gmail.com>
2024-09-16 11:30:16 +0530

    Your commit message

--- src/index.js
+++ src/index.js
@@ -6,7 +6,15 @@ dotenv.config({
     path: "./env"
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at: ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed ", err)
+})
 
 /*
 This is another method to connect database
